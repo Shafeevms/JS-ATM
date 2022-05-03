2d428546a476e6f8380987434c18a293db8f2d2c
Shafeevms <Shafeevms@gmail.com>
2022-05-03 13:03:52 +0300

    cypress install

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -3,7 +3,7 @@
 test('1 + 1 = 2', () => {
   expect(1 + 1).toBe(2);
 });
-
+//! не понимаю как создавать мок для этой функции
 // test('запрос авторизации на сервис', () => {
 //   const correctLogin = { login: 'developer', password: 'skillbox' };
 //   expect(fetchLogin(correctLogin)).toBe(
--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -1,6 +1,9 @@
 import { formDataToObject } from '../src/js/helpers';
 
-jest.mock('../__mocks__/mock.js');
+//! хочу импортировать мок с объектом - как в документации не работет
+//! если импортировать обычным импортом jest не понимает new FomData();
+
+jest.mock('../__mocks__/formdata.js');
 
 test('функция вовзращает из formData объект', () => {
   expect(formDataToObject(testForm)).toBe({
